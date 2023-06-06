877ca09736578c7f7c0acf88cc4c65cbd27adaf2
tyler-radke1 <109247761+tyler-radke1@users.noreply.github.com>
2023-06-06 13:16:46 -0600

    Initial Commit

---
+++ Qr Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Qr Test
+//
+//  Created by Tyler Radke on 6/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Qr Test/Qr_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qr_TestApp.swift
+//  Qr Test
+//
+//  Created by Tyler Radke on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Qr_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
